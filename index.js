b33a79b3e05991c40414a1950ac10cedb461e991
Aldo D'Aquino <daquino.aldo@gmail.com>
2019-03-11 15:00:48 +0100

    First upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const httpProxy = require("http-proxy")
+const http = require("http")
+const url = require("url")
+
+const proxy = httpProxy.createProxyServer()
+
+const server = http.createServer((req, res) => {
+  const query = url.parse(req.url, true).query
+  if (query.url)
+    proxy.web(req, res, { target: query.url }, e => {
+      res.writeHead(400, { 'Content-Type': 'text/plain' })
+      res.end("Invalid url " + query.url + ".")
+    })
+  else {
+    res.writeHead(400, { 'Content-Type': 'text/plain' })
+    res.end("Missing url!\nUsage: /?url=http://url-to.proxy")
+  }
+}).listen(8080)
